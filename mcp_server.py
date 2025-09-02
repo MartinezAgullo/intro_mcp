e76dcdf4c1b5b09ecb40cccd41686f82d238e478
MartinezAgullo <pablo.martinez.agullo@gmail.com>
2025-09-02 19:03:26 +0200

    feat: Include resources

--- mcp_server.py
+++ mcp_server.py
@@ -1,4 +1,6 @@
 from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
 
@@ -13,9 +15,9 @@ docs = {
 }
 
 # Tool to read a doc
-@mcp.add_tool(
+@mcp.tool(
     name="read_doc_contents",
-    description="Read the content of a document an retun it as a string."
+    description="Read the content of a document an retun it as a string.",
 )
 def read_document(
     doc_id: str = Field(description="Id of the document to read"),
@@ -45,8 +47,19 @@ def edit_document(
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+# Resource to return all doc id's
+@mcp.resource("docs://documents", mime_type="application/json") # Direct resource
+def list_docs() -> list[str]:
+    return list(docs.keys())
+
+
+# Resource to return the contents of a particular doc
+@mcp.resource("docs://documents/{doc_id}", mime_type="text/plain") # Templated resorce
+def fetch_doc(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"Document with id {doc_id} not found")
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
